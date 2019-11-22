241834546d6a87f67788bc2cc35d14ca2014bde4
fidr <f1gp3a@gmail.com>
2019-11-22 08:14:06 +0000

    update

--- js/actadult.js
+++ js/actadult.js
@@ -11,11 +11,11 @@ button.addEventListener("click", function(){
     total = +(one+two+three+four+five);
     document.getElementById("message").textContent = "Total score = " + total;
     if (total < 15){
-        document.getElementById("level").textContent = "Poorly or uncontrolled asthma 哮喘病差或无法控制";
+        document.getElementById("level").textContent = "Poorly or uncontrolled asthma 您的哮喘未受到控制。請立即和您的醫護人員聯絡。";
     } else if (total <= 19) {
-        document.getElementById("level").textContent = "Somewhat controlled asthma 某程度上哮喘收控制";
+        document.getElementById("level").textContent = "Somewhat controlled asthma 您的哮喘可能未達致良好受控的程度。請與醫護人員商討如何協助改善控制哮喘的方法。";
     } else{
-        document.getElementById("level").textContent = "Well or completely controlled asthma 哮喘完全收控制";
+        document.getElementById("level").textContent = "Well or completely controlled asthma 您的哮喘暫時控制良好。然而，哮喘受控的情況可能隨時有變動，因此建議您定期進行本測試, 和醫護人員一起檢視您的哮喘控制情況。";
     };
 });
 

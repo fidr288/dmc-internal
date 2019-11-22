4f620bac5e83a8e22ecaec51167e52995f49fbc2
fidr <f1gp3a@gmail.com>
2019-11-22 03:11:04 +0000

    update

--- js/actchild.js
+++ js/actchild.js
@@ -8,8 +8,8 @@ button.addEventListener("click", function(){
     let three = parseInt(document.querySelector('input[name="three"]:checked').value);
     let four = parseInt(document.querySelector('input[name="four"]:checked').value);
     let five = parseInt(document.querySelector('input[name="five"]:checked').value);
-    let five = parseInt(document.querySelector('input[name="five"]:checked').value);
-    let five = parseInt(document.querySelector('input[name="five"]:checked').value);
+    let six = parseInt(document.querySelector('input[name="six"]:checked').value);
+    let seven = parseInt(document.querySelector('input[name="seven"]:checked').value);
     total = +(one+two+three+four+five+six+seven);
     document.getElementById("message").textContent = "Total score = " + total;
     if (total < 15){
